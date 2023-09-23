2f88ff0a52d6488c6fbac7ba3105e4af2f14ef38
Alpha01 <www.aman.mk.1234@gmail.com>
2023-09-23 19:59:15 +0530

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
+Hello there ,
+The project uses react expo which is still under development and will face certain issues while deploying the app.
+
+To run this project-
+
+1. login to rapid API , get a JSearch api key and paste it in
+   "X-RapidAPI-Key": '<yourapikey>',
+
+2. 'exp start --tunnel' in your command line and follow the instructions.
+
+3. Download the Expo app in your android phone and scan the QR code.
+
 # Expo Router Example
 
 Use [`expo-router`](https://expo.github.io/router) to build native navigation using files in the `app/` directory.
---
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -0,0 +1,34 @@
+
+import { View, Text ,TouchableOpacity, Image} from 'react-native'
+
+import styles from './popularjobcard.style'
+import { checkImageURL } from '../../../../utils'
+
+const PopularJobCard = ({item , selectedjob , handleCardPress}) => {
+  return (
+    <TouchableOpacity 
+      style={styles.container(selectedjob,item)}
+      onPress={()=>handleCardPress(item)}
+      >
+      <TouchableOpacity style={styles.logoContainer(selectedjob,item)}>
+        <Image source={{uri: checkImageURL(item.employer_logo) ? item.employer_logo : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg"}}
+          resizeMode='contain'
+          style={styles.logoImage}
+      />
+      </TouchableOpacity>
+    <Text style={styles.companyName} numberOfLines={1}>{item.employer_name}</Text>
+
+    <View style={styles.infoContainer}>
+      <Text style={styles.jobName(selectedjob,item)} numberOfLines={1}>
+        {item.job_title}
+      </Text>
+      <Text style={styles.location}>
+        {item.job_country}
+      </Text>
+    </View>
+
+    </TouchableOpacity>
+  )
+}
+
+export default PopularJobCard;
